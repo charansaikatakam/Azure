819bd730f1b0e9a12f80c6f53911d08199acdf12
charansaikatakam <charansaikatakam@students.vnit.ac.in>
2025-02-02 16:46:45 +0530

    bugfixed

--- IAAC/terraform/networking/HubNetworking.tf
+++ IAAC/terraform/networking/HubNetworking.tf
@@ -10,7 +10,7 @@ resource "azurerm_subnet" "subnets" {
   for_each = var.SubnetNamesAndAddressSpaces
   name                 = each.key
   resource_group_name  = azurerm_resource_group.devRGHub.name
-  virtual_network_name = azurerm_virtual_network.vnetWeb.name
+  virtual_network_name = azurerm_virtual_network.vnetHub.name
   address_prefixes     = each.value
 }
 
